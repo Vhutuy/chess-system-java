31debb22b9d491eeb51a77972ebac39d71b3b811
Vitor <vitorsobrado7@gmail.com>
2023-10-06 16:44:18 -0300

    Player switch in wich turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,11 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Color getcurrentPlayer() {
+		return currentPlayer;
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -43,6 +55,7 @@ public class ChessMatch {
 			validateTargetPosition(initial, target);
 			
 			Piece capturedPiece = makeMove(initial, target);
+			nextTurn();
 			return (ChessPiece)capturedPiece;
 	}
 	
@@ -62,9 +75,17 @@ public class ChessMatch {
 		
 	}
 	
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+	}
+	
 	private void validateSourcePosition(Position position) {
 		if(!board.threIsAPiece(position)) {
 			throw new ChessExceptions("There is no piece in source position!");
+		}
+		if (currentPlayer != ((ChessPiece) board.piece(position)).getColor()) {
+			throw new ChessExceptions("The chosen piece is not yours!");
 		}
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessExceptions("There is no possible moves for this piece!");
