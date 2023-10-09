46f2ac8d8018e87a24640596045b919a633d1813
Vitor <vitorsobrado7@gmail.com>
2023-10-09 16:30:01 -0300

    Pawn moviment

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -197,12 +198,30 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
-		placePiece('h', 7, new Rook(board, Color.WHITE));
-		placePiece('d', 1, new Rook(board, Color.WHITE));
+		placePiece('a', 1, new Rook(board, Color.WHITE));
 		placePiece('e', 1, new King(board, Color.WHITE));
-
-		placePiece('b', 8, new Rook(board, Color.BLACK));
-		placePiece('a', 8, new King(board, Color.BLACK));
+		placePiece('h', 1, new Rook(board, Color.WHITE));
+		placePiece('a', 2, new Pawn(board, Color.WHITE));
+		placePiece('b', 2, new Pawn(board, Color.WHITE));
+		placePiece('c', 2, new Pawn(board, Color.WHITE));
+		placePiece('d', 2, new Pawn(board, Color.WHITE));
+		placePiece('e', 2, new Pawn(board, Color.WHITE));
+		placePiece('f', 2, new Pawn(board, Color.WHITE));
+		placePiece('g', 2, new Pawn(board, Color.WHITE));
+		placePiece('h', 2, new Pawn(board, Color.WHITE));
+		
+		
+		placePiece('a', 8, new Rook(board, Color.BLACK));
+		placePiece('e', 8, new King(board, Color.BLACK));
+		placePiece('h', 8, new Rook(board, Color.BLACK));
+		placePiece('a', 7, new Pawn(board, Color.BLACK));
+		placePiece('b', 7, new Pawn(board, Color.BLACK));
+		placePiece('c', 7, new Pawn(board, Color.BLACK));
+		placePiece('d', 7, new Pawn(board, Color.BLACK));
+		placePiece('e', 7, new Pawn(board, Color.BLACK));
+		placePiece('f', 7, new Pawn(board, Color.BLACK));
+		placePiece('g', 7, new Pawn(board, Color.BLACK));
+		placePiece('h', 7, new Pawn(board, Color.BLACK));
 
 	}
 
